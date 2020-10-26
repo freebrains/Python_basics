e4c298fe27237d6f4abc9dc62afb89fa1a4160cd
freebrains <slimjeem@gmail.com>
2020-10-26 17:15:16 +0300

    Modified task 5 from lesson 1

--- Lesson1.5.py
+++ Lesson1.5.py
@@ -1,3 +1,22 @@
+'''
+Запросите у пользователя значения выручки и издержек фирмы.
+Определите, с каким финансовым результатом работает фирма
+(прибыль — выручка больше издержек, или убыток — издержки
+больше выручки). Выведите соответствующее сообщение. Если
+фирма отработала с прибылью, вычислите рентабельность выручки
+(соотношение прибыли к выручке). Далее запросите численность
+сотрудников фирмы и определите прибыль фирмы в расчете на
+одного сотрудника.
+Request from the user amount of revenue and costs. Determine
+the financial result of the company (profit - revenue is
+greater than costs, or loss - costs are greater than revenue).
+Display appropriate message. If the company has worked with
+a profit, calculate the return on revenue (the ratio of
+profit to revenue). Next, ask for the number of employees
+and determine the company's profit per employee.
+'''
+
+
 a = int(input("Enter income - "))
 b = int(input("Enter expenses - "))
 if a < b:
