360fd106299a6751f10a3e154af316fcf3c59d0a
freebrains <slimjeem@gmail.com>
2020-10-26 15:17:06 +0300

    Modofied second task for lesson 1

--- Lesson1.2.py
+++ Lesson1.2.py
@@ -1,3 +1,12 @@
+'''
+Пользователь вводит время в секундах. Переведите время в часы,
+минуты и секунды и выведите в формате чч:мм:сс. Используйте
+форматирование строк.
+The user enters the time in seconds. Transform the time in
+hours, minutes and seconds and display like hh:mm:ss. Use
+string formatting.
+'''
+
 time = int(input("Enter seconds"))
 hours = time // 3600
 minutes = (time - hours * 3600) // 60
